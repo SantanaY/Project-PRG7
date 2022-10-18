f0056f64def44c2845a986237a9460221e4c721a
Ernst-L <90588151+Ernst-L@users.noreply.github.com>
2022-10-18 20:54:16 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+// #include <vector>
 #include <cstring>
 
 
@@ -37,3 +37,85 @@ struct log_project
 	int kuantiti;
 };
 
+int main()
+{
+
+	int item_count = -1;
+	int pegawai_count = -1;
+	int log_count = -1;
+	std::string top_line;
+	std::string s_temp;
+
+	// input file item.txt
+	std::ifstream item_istream("./data/item.txt");
+
+	while (std::getline(item_istream, top_line)) item_count++;
+	item_project arr_item[item_count];
+
+	item_istream.clear();
+	item_istream.seekg(0);
+	std::getline(item_istream, top_line);
+	for (int i = 0; i < item_count; i++)
+	{
+		std::getline(item_istream, s_temp, '\t');
+		strcpy(arr_item[i].id, s_temp.c_str());
+		std::getline(item_istream, s_temp, '\t');
+		strcpy(arr_item[i].nama, s_temp.c_str());
+		std::getline(item_istream, s_temp, '\t');
+		strcpy(arr_item[i].sku, s_temp.c_str());
+		std::getline(item_istream, s_temp, '\t');
+		arr_item[i].harga = stoi(s_temp);
+		std::getline(item_istream, s_temp, '\t');
+		arr_item[i].diskon_nominal = stoi(s_temp);
+		std::getline(item_istream, s_temp, '\t');
+		arr_item[i].diskon_persen = stoi(s_temp);
+		std::getline(item_istream, s_temp);
+		arr_item[i].stok = stoi(s_temp);
+	}
+	item_istream.close();
+
+	// input file pegawai.txt
+	std::ifstream pegawai_istream("./data/pegawai.txt");
+
+	while (std::getline(pegawai_istream, top_line)) pegawai_count++;
+	pegawai_project arr_pegawai[pegawai_count];
+
+	pegawai_istream.clear();
+	pegawai_istream.seekg(0);
+	std::getline(pegawai_istream, top_line);
+	for (int i = 0; i < pegawai_count; i++)
+	{
+		std::getline(pegawai_istream, s_temp, '\t');
+		strcpy(arr_pegawai[i].id, s_temp.c_str());
+		std::getline(pegawai_istream, s_temp);
+		strcpy(arr_pegawai[i].nama, s_temp.c_str());
+	}
+	pegawai_istream.close();
+
+	// input file log.txt
+	std::ifstream log_istream("./data/log.txt");
+
+	while (std::getline(log_istream, top_line)) log_count++;
+	log_project arr_log[log_count];
+
+	log_istream.clear();
+	log_istream.seekg(0);
+	std::getline(log_istream, top_line);
+	for (int i = 0; i < log_count; i++)
+	{
+		std::getline(log_istream, s_temp, '\t');
+		strcpy(arr_log[i].time, s_temp.c_str());
+		std::getline(log_istream, s_temp, '\t');
+		strcpy(arr_log[i].id_kasir, s_temp.c_str());
+		std::getline(log_istream, s_temp, '\t');
+		strcpy(arr_log[i].id_konsumen, s_temp.c_str());
+		std::getline(log_istream, s_temp, '\t');
+		strcpy(arr_log[i].id_item, s_temp.c_str());
+		std::getline(log_istream, s_temp, '\t');
+		arr_log[i].harga = stoi(s_temp);
+		std::getline(log_istream, s_temp, '\t');
+		arr_log[i].diskon = stoi(s_temp);
+		std::getline(log_istream, s_temp);
+		arr_log[i].kuantiti = stoi(s_temp);
+	}
+}
